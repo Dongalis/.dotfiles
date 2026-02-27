12809dcc62cb0977510896aadd3edec88d468e7e
Dongalis <47064326+Dongalis@users.noreply.github.com>
2026-02-27 19:34:11 +0100

    Adding group sorting to keybinding menu

--- elephant/.config/elephant/menus/keybindings.lua
+++ elephant/.config/elephant/menus/keybindings.lua
@@ -3,6 +3,57 @@ NamePretty = "Key Bindings"
 --Icon = "icon-name"
 FixedOrder = true
 local separator = " + "
+--local grp_prefix = "                "
+
+local groups = {
+    {
+        name = "Hyprland",
+        regex = {"Launch", "Close window", "Toggle window", "Pseudo window", "key bindings"},
+        entries = {}
+    },
+    {
+        name = "Group Control",
+        regex = {"group"},
+        entries = {}
+    },
+    {
+        name = "Notifications",
+        regex = {"notification"},
+        entries = {}
+    },
+    {
+        name = "Wondow / Workspace actions",
+        regex = {"workspace"},
+        entries = {}
+    },
+    {
+        name = "Audio",
+        regex = {"Volume", "Track", "audio"},
+        entries = {}
+    },
+    {
+        name = "Miscellaneous",
+        regex = {},
+        entries = {}
+    }
+}
+
+local function format_entry(modmask, key, description)
+    return { Text = description, Subtext = table.concat(modmask) .. key }
+end
+
+local function insert_into_group(modmask, key, description)
+    local sub = description:lower()
+    for _, group in ipairs(groups) do
+        for _, pattern in ipairs(group.regex) do
+            if sub:match(pattern:lower()) then
+                table.insert(group.entries, format_entry(modmask, key, description))
+                return
+            end
+        end
+    end
+    table.insert(groups[#groups].entries, format_entry(modmask, key, description))
+end
 
 local function hasbit(x, b)
     return math.floor(x / b) % 2 == 1
@@ -21,6 +72,8 @@ function GetEntries()
     local modmask = {}
     local key = ""
     local description = ""
+--    local dispatcher = ""
+--    local args = ""
     while true do
         local line = handle:read()
         if line == nil then break end
@@ -43,22 +96,36 @@ function GetEntries()
        
         if line == "" then
             if key ~= "" then
-                local tmp = table.concat(modmask) .. key
-                table.insert(entries, { Text = tmp, Subtext = description})
+                insert_into_group(modmask, key, description)
                 modmask = {}
                 key = ""
                 description = ""
+--                dispatcher = ""
+--                args = ""
             end
         end
 
+    end
+
+    handle:close()
+    
+    local final = {}
+
+    for _, group in ipairs(groups) do
+        if #group.entries > 0 then
+           --table.insert(final, { Text = grp_prefix .. group.name,})
+
+            for _, entry in ipairs(group.entries) do
+                table.insert(final, entry)
+            end
+        end
     end
 
 --    for line in handle:lines() do
 --        table.insert(entries, {Text = line, Actions = {}})
 --    end
 
-    handle:close()
-    return entries
+    return final
 end
 
 --function MyAction()
