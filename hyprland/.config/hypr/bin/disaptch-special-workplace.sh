468a3935acb6687020d9ba6f1e5409811a8c30d3
Dongalis <47064326+Dongalis@users.noreply.github.com>
2026-06-17 21:59:35 +0200

    Fixing special workspaces script

--- hyprland/.config/hypr/bin/disaptch-special-workplace.sh
+++ hyprland/.config/hypr/bin/disaptch-special-workplace.sh
@@ -18,22 +18,20 @@ CURRENT_WORKSPACE=$(hyprctl activeworkspace -j | jq '.id')
 WINDOW_WORKSPACE=$(hyprctl clients -j | jq -r --arg class "$CLASS" '.[] | select(.class == $class) | .workspace.name')
 
 if [[ -z "${WINDOW_WORKSPACE:-}" ]]; then
-    hyprctl dispatch togglespecialworkspace "$WORKSPACE"
+    hyprctl dispatch "hl.dsp.workspace.toggle_special('$WORKSPACE')"
     exit 0
 
 elif [[ "$WINDOW_WORKSPACE" == "special:$WORKSPACE" ]]; then
-    hyprctl dispatch togglespecialworkspace "$WORKSPACE"
+    hyprctl dispatch "hl.dsp.workspace.toggle_special('$WORKSPACE')"
 
 else
-    hyprctl dispatch focuswindow class:$CLASS
-    hyprctl dispatch movetoworkspace $CURRENT_WORKSPACE
-    hyprctl dispatch movetoworkspace special:$WORKSPACE
+    hyprctl dispatch "hl.dsp.window.move({workspace='special:$WORKSPACE',window={class=$CLASS}})"
 fi
 
 sleep 0.01
 
 if [[ "$CLASS" == "$(hyprctl activewindow -j | jq -r '.class')" ]] then
-    hyprctl dispatch fullscreenstate 0
+    hyprctl dispatch 'hl.dsp.window.fullscreen_state0'
     hyprctl dispatch settiled
 fi
 
